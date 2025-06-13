950e21fae13fb7e8fecdc9b54c26ced9811b5481
Juma Kassim <jumakassim89@gmail.com>
2025-06-13 09:29:46 +0300

    config

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,13 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3009'], // or '*' if no credentials
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3039',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:3039',
+    ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
